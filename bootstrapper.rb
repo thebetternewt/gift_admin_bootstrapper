6e7915f06ff1e5f6009ed8ad9c865d5e64e55e4a
Christopher R. Eady <chris.eady1@gmail.com>
2017-06-21 16:56:27 -0500

    Added excel add-in install to boostrapper

--- bootstrapper.rb
+++ bootstrapper.rb
@@ -7,6 +7,8 @@ git_repos = Hash[
   end
 ]
 
+complete = "Complete! you may now close this window. (Unless there are errors...)"
+
 # git_repos.each { |l| puts l } # DEBUG INFO
 
 if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM # Check if Windows OS
@@ -20,6 +22,7 @@ if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM # Check if Windows OS
   STDIN.getc
   sleep(0.2)
 
+  # Install/Update Ruby Scripts
   script_directories.select do |directory|
     path = '\\Ruby Scripts\\' + directory
     puts "Searching for: #{path}"
@@ -28,7 +31,7 @@ if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM # Check if Windows OS
       puts "Found #{directory}!"
       puts "Preparing to update git repository (new window)..."
       sleep(0.5)
-      system %{ start cmd /k "cd #{path} && git pull" }
+      system %{ start cmd /k "cd #{path} && git pull && echo. && echo #{complete}" }
     elsif Dir.exist?(path)
       puts "Found #{directory}!"
       puts "Not a git repository...removing directory and contents..."
@@ -36,20 +39,31 @@ if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM # Check if Windows OS
       FileUtils.cd('\Ruby Scripts')
       puts "Preparing to clone new repository (new window)..."
       sleep(0.5)
-      system %{ start cmd /k "cd \\Ruby Scripts & git clone #{git_repos[directory]}" }
+      system %{ start cmd /k "cd \\Ruby Scripts & git clone #{git_repos[directory]} && echo. && echo #{complete}" }
     else
       puts "Did not find #{directory}!"
       puts "Preparing to clone repository (new window)..."
       sleep(0.5)
-      system %{ start cmd /k "cd \\Ruby Scripts & git clone #{git_repos[directory]}" }
+      system %{ start cmd /k "cd \\Ruby Scripts & git clone #{git_repos[directory]} && echo. && echo #{complete}" }
     end
   end
+
+  # Install Excel Plugin
+  appdata_path = Dir.home + '\\AppData\Roaming\Microsoft\AddIns'
+  puts
+  puts "Validating add-in directory: " + appdata_path
+  sleep(0.2)
+  FileUtils.mkdir_p(appdata_path) unless Dir.exist?(appdata_path)
+  puts
+  puts "Installing GiftAdminReports.xlam to " + appdata_path
+  sleep(0.2)
+  FileUtils.cp( './excel_add_in/GiftAdminReports.xlam', appdata_path )
+  puts "GiftAdminReports.xlam installed! Add-in can now be activated in Excel settings."
+  puts
+  puts "Install complete!"
 
 else system %{cd /} # Assume Mac OS/Linux
   # Not yet implemented.
   puts "Please install scripts manually. Not yet implemented for *NIX systems."
 end
-
-
-
 
